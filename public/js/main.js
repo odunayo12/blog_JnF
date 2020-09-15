c47dacbccbc47b128d994350ec7c5774b6264f16
odunayo12 <odun.rotimi@gmail.com>
2020-09-15 14:59:55 +0200

    MongoDB gone live

--- public/js/main.js
+++ public/js/main.js
@@ -1,80 +1,98 @@
- $(document).ready(
-     function() {
-         //compose page
-         // checkbox control flow
+$(document).ready(function () {
+  //compose page
+  // checkbox control flow
 
+  //  Radio buttons control flow
+  let sectionButtons = $("#conditionalForm input:radio[name = sectionRadios]");
 
-         //  Radio buttons control flow
-         //  let sectionButtons = $('#conditionalForm input:radio[name = sectionRadios]');
+  let authorDetails = $("#conditionalForm #authorDetails");
+  let postTitleNTags = $("#conditionalForm #postTitleNTags");
+  let introTextArea = $(
+    '#conditionalForm textarea[name = "introTextArea"]'
+  ).parent();
+  let linkTextAtrea = $(
+    '#conditionalForm textarea[name = "linkTextAtrea"]'
+  ).parent();
+  let postTextAtrea = $(
+    '#conditionalForm textarea[name = "postTextAtrea"]'
+  ).parent();
+  let imageLinkText = $(
+    '#conditionalForm textarea[name = "imageLinkText"]'
+  ).parent();
+  let postSubmitButton = $("#conditionalForm #postSubmitButton").parent();
 
+  let completeForm = authorDetails
+    .add(postTitleNTags)
+    .add(introTextArea)
+    .add(linkTextAtrea)
+    .add(postTextAtrea)
+    .add(imageLinkText)
+    .add(postSubmitButton);
+  let bookForm = authorDetails
+    .add(postTitleNTags)
+    .add(introTextArea)
+    .add(linkTextAtrea)
+    .add(imageLinkText)
+    .add(postSubmitButton);
+  let vedAudioForm = authorDetails
+    .add(postTitleNTags)
+    .add(introTextArea)
+    .add(linkTextAtrea)
+    .add(postSubmitButton);
+  let articleForm = authorDetails
+    .add(postTitleNTags)
+    .add(introTextArea)
+    .add(postTextAtrea)
+    .add(postSubmitButton);
 
+  sectionButtons.change(function () {
+    var value = this.value;
+    completeForm.addClass("hidden");
 
-         //  let authorDetails = $('#conditionalForm #authorDetails');
-         //  let postTitleNTags = $('#conditionalForm #postTitleNTags');
-         //  let introTextArea = $('#conditionalForm textarea[name = "introTextArea"]').parent();
-         //  let linkTextAtrea = $('#conditionalForm textarea[name = "linkTextAtrea"]').parent();
-         //  let postTextAtrea = $('#conditionalForm textarea[name = "postTextAtrea"]').parent();
-         //  let imageLinkText = $('#conditionalForm textarea[name = "imageLinkText"]').parent();
+    if (value == "Article") {
+      articleForm.removeClass("hidden");
+    } else if (value == "Book") {
+      bookForm.removeClass("hidden");
+      $("label[for = 'postTitle']").text("Book Title");
+    } else {
+      vedAudioForm.removeClass("hidden");
+      $("label[for = 'postTitle']").text("Vedio Title");
+    }
+  });
 
-         //  let completeForm = authorDetails.add(postTitleNTags).add(introTextArea).add(linkTextAtrea).add(postTextAtrea).add(imageLinkText);
-         //  let bookForm = authorDetails.add(postTitleNTags).add(introTextArea).add(linkTextAtrea).add(imageLinkText);
-         //  let vedAudioForm = authorDetails.add(postTitleNTags).add(introTextArea).add(linkTextAtrea);
-         //  let articleForm = authorDetails.add(postTitleNTags).add(introTextArea).add(postTextAtrea);
+  //post page
 
-         //  sectionButtons.change(function() {
-         //      var value = this.value;
-         //      completeForm.addClass('hidden');
+  $("#replyForm").on("submit", (e) => {
+    e.preventDefault();
 
-         //      if (value == "Article") {
-         //          articleForm.removeClass('hidden');
-         //      } else if (value == "Book") {
-         //          bookForm.removeClass('hidden');
-         //          $("label[for = 'postTitle']").text('Book Title');
-         //      } else {
-         //          vedAudioForm.removeClass('hidden');
-         //          $("label[for = 'postTitle']").text('Vedio Title')
-         //      }
-         //  });
-         //post page
+    const senderName = $("#replyFormName").val().trim();
+    const senderEMail = $("#replyFormEmail").val().trim();
+    const senderComment = $("#replyFormComment").val().trim();
 
-         $('#replyForm').on('submit', (e) => {
-             e.preventDefault();
+    const replyFormData = {
+      senderName,
+      senderEMail,
+      senderComment,
+    };
+    $.post("/posts", replyFormData, () => {
+      console.log("Server Recieved Message");
+    });
+  });
 
-             const senderName = $('#replyFormName').val().trim();
-             const senderEMail = $('#replyFormEmail').val().trim();
-             const senderComment = $('#replyFormComment').val().trim();
+  $(".chips").on("chip.add", function (e, chip) {
+    // you have the added chip here
+  });
 
-             const replyFormData = {
-                 senderName,
-                 senderEMail,
-                 senderComment
-             }
-             $.post('/posts', replyFormData, () => {
-                 console.log('Server Recieved Message')
-             })
+  // enhance multiple select in post page
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    $(".selectpicker").selectpicker("mobile");
+  }
 
-         });
+  //animated nav-bar icon
+  $(".first-button").on("click", function () {
+    $(".animated-icon1").toggleClass("open");
+  });
 
-         $('.chips').on('chip.add', function(e, chip) {
-             // you have the added chip here
-         });
-
-         // enhance multiple select in post page
-         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-             $('.selectpicker').selectpicker('mobile');
-         }
-
-
-         //animated nav-bar icon
-         $('.first-button').on('click', function() {
-
-             $('.animated-icon1').toggleClass('open');
-         });
-
-
-
-
-
-         // Animations initialization
-         new WOW().init();
-     });
+  // Animations initialization
+  new WOW().init();
+});
